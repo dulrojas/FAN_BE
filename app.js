6b1163b48682a615a89ffb3af1e6e761751f3e08
Angel Bruno Laura Calliconde <angel.laura@CI-LAPAZ.GOV.BO>
2024-11-13 15:13:29 -0400

    ajustes archivos, limpieza de app.js

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ const app = express();
 app.use(cors());
 app.options('*', cors());
 
-const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
-const OAuth2 = google.auth.OAuth2;
+// const nodemailer = require("nodemailer");
+// const { google } = require("googleapis");
+// const OAuth2 = google.auth.OAuth2;
 
 // These id's and secrets should come from .env file.
-const accountTransport = require("./account_transport.json");
-const CLIENT_ID = accountTransport.auth.clientId;
-const CLEINT_SECRET = accountTransport.auth.clientSecret;
-const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = accountTransport.auth.refreshToken;
+// const accountTransport = require("./account_transport.json");
+// const CLIENT_ID = accountTransport.auth.clientId;
+// const CLEINT_SECRET = accountTransport.auth.clientSecret;
+// const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+// const REFRESH_TOKEN = accountTransport.auth.refreshToken;
 
 const port = 3005;
 const q = require('./queries');
@@ -28,21 +28,21 @@ app.use(
   })
 )
 
-const oAuth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLEINT_SECRET,
-  REDIRECT_URI
-);
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+// const oAuth2Client = new google.auth.OAuth2(
+//   CLIENT_ID,
+//   CLEINT_SECRET,
+//   REDIRECT_URI
+// );
+// oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
-app.post('/email', (req, res) => {
-  // sendEM(0,function(rescb){
-  //   console.log(rescb);
-  //   res.send(rescb);
-  // })
-  console.log("email");
-  sendMail(res);
-});
+// app.post('/email', (req, res) => {
+//   // sendEM(0,function(rescb){
+//   //   console.log(rescb);
+//   //   res.send(rescb);
+//   // })
+//   console.log("email");
+//   sendMail(res);
+// });
 
 app.get('/', (req, res) => {
   res.send('No tiene permisos para el acceso.');
@@ -56,20 +56,20 @@ app.post('/consultas', q.Consultas);
 app.post('/sp', q.getSP);
 app.post('/uploadFile', f.uploadMiddleware, f.handleFileUpload);
 app.post('/downloadFile', f.uploadMiddleware, f.handleFileDownload);
-app.get('/downloadFile/:id', f.uploadMiddleware, f.handleFileDownload2);
-app.post('/reporte/expensasResponsable', (req, res) => {
-  r.expensasResponsable(req,buffer => {
+// app.get('/downloadFile/:id', f.uploadMiddleware, f.handleFileDownload2);
+// app.post('/reporte/expensasResponsable', (req, res) => {
+//   r.expensasResponsable(req,buffer => {
     
-    console.log(buffer);
-    if(buffer=="ERROR"){
+//     console.log(buffer);
+//     if(buffer=="ERROR"){
       
-      return res.status(500).json("Error al generar pdf");
-    }
+//       return res.status(500).json("Error al generar pdf");
+//     }
 
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'attachment; filename=reporte.pdf');
-    res.send(buffer);
-  });
-});
+//     res.setHeader('Content-Type', 'application/pdf');
+//     res.setHeader('Content-Disposition', 'attachment; filename=reporte.pdf');
+//     res.send(buffer);
+//   });
+// });
 
 app.listen(port, () => console.log('aplicacion en puerto '+ port));
